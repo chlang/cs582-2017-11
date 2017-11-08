e91590b8f65f2f6fe5de5ac4d00eace8bb6300b9
ortigali <ortigali.bazarov@gmail.com>
2017-11-08 01:11:22 -0600

    just comment

--- src/MachineLearning/src/Labs/Lab5/Prob2/test.py
+++ src/MachineLearning/src/Labs/Lab5/Prob2/test.py
@@ -12,3 +12,16 @@ p.confmat(x, y)
 
 
 print(w)
+# """"
+# since we proved from the problem 1 that x+y=3/2 is separating vector
+# from the several training of perceptron we get trained output value of w
+# [[ 0.53248878]
+#  [ 0.5161737 ]
+#  [ 0.73048374]]
+# w[0]x+w[1]y = w[3]
+# => 0.5x+0.5y = 0.7
+# => x + y = 7/5
+# 7/5 ≈ 3/2
+# => 14/10 ≈ 15/10
+# So the perceptron returns almost same result which we found manually in #1 problem
+# """"
